9f3c1fbd40fdededa59df8a6f5e5f838f1cbfeb2
paung29 <aikepaung767@gmail.com>
2025-09-14 15:24:28 +0700

    first

---
+++ hello-go/hello.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main()  {
+
+	var student1 string= "John"
+	var student2 = "hi"
+
+
+	fmt.Println("Hello world")
+	fmt.Println(student1)
+	fmt.Println(student2)
+}
